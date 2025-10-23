54993887554136881a6bd7a7a4bfa75844c0b32b
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-23 15:02:46 +0200

    Add fetch call

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,10 @@
  * Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
  */
 
+function printListItem(element, item) {
+	element.innerHTML += `<li>${item}</li>`;
+}
+
 // Select email-list from html
 const emailListElement = document.getElementById("email-list");
 
@@ -17,6 +21,15 @@ const emailListElement = document.getElementById("email-list");
 const randomMailEndpoint =
 	"https://flynn.boolean.careers/exercises/api/random/mail";
 
-// For each
-// - build a list item
-// - append to email-list element
+// With fetch
+for (let i = 0; i < 10; i++) {
+	fetch(randomMailEndpoint)
+		.then((response) => response.json())
+		.then((result) => {
+			const { success, response } = result;
+			if (success) {
+				console.log(response);
+				printListItem(emailListElement, response);
+			}
+		});
+}
